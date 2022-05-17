c8f8a4038e88ee9758b81ee473e69f09fd2c21ca
강성훈 <80798929+ksh5324@users.noreply.github.com>
2022-05-17 19:25:50 +0900

    page (#7)

--- frontend/src/components/choose/chooseStyle.ts
+++ frontend/src/components/choose/chooseStyle.ts
@@ -1,30 +1,60 @@
 import styled from "styled-components";
 
 export const ChooseContainer = styled.div`
-  width: 750px;
-  display: flex;
-  justify-content: space-between;
+  width: 1104px;
+  height: 579px;
+  position: relative;
+  border: 4px solid transparent;
+  border-radius: 40px;
+  background-image: linear-gradient(#fff, #fff),
+    linear-gradient(90deg, #10c500 0%, #003bad 65.63%);
+  background-origin: border-box;
+  background-clip: content-box, border-box;
 
-  button {
-    width: 325px;
-    height: 125px;
-    background: #ffffff;
-    box-shadow: 4px 4px 15px rgba(1, 0, 0, 0.15);
-    border-radius: 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  .des {
+    margin-top: 59px;
+    width: 723px;
+    border-bottom: 3px solid black;
+    text-align: center;
+    padding-bottom: 9px;
     font-family: "Inter";
     font-style: normal;
-    font-weight: 400;
+    font-weight: 800;
     font-size: 30px;
     line-height: 36px;
-    color: #000000;
-    transition: 0.15s;
-    cursor: pointer;
+    text-align: center;
 
-    &:hover {
-      background: #1556f7;
-      color: white;
-      transition: 0.15s;
-      transform: scale(1.05);
-    }
+    color: #000000;
+  }
+  .button-con {
+    width: 723px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-top: 47px;
+  }
+  button {
+    background: #e9eaef;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 30px;
+    width: 189px;
+    cursor: pointer;
+    height: 144px;
+    font-family: "Inter";
+    font-style: normal;
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 19px;
+    color: #000000;
+    transition: 0.5s;
+  }
+  button:hover {
+    background: #d9dadf;
+    font-size: 17px;
   }
 `;
