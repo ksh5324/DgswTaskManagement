49540cbf0689fbd17d5814acede5d350002ea33b
강성훈 <80798929+ksh5324@users.noreply.github.com>
2022-05-23 17:59:06 +0900

    task (#18)

--- frontend/src/components/table/Task.tsx
+++ frontend/src/components/table/Task.tsx
@@ -2,7 +2,38 @@ import React from "react";
 import { TaskContainer } from "./tableStyle";
 
 const Task = () => {
-  return <TaskContainer></TaskContainer>;
+  return (
+    <TaskContainer>
+      <div className="main">
+        <details>
+          <summary>
+            국어
+            <div className="summary-chevron-up">
+              <svg
+                width="21"
+                height="21"
+                viewBox="0 0 21 21"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M21 3L0 3V0L21 0V3ZM10.5 21L20.505 6L0.494999 6L10.5 21Z"
+                  fill="#003AAD"
+                />
+              </svg>
+            </div>
+          </summary>
+          <ul>
+            <li>ppt 제작</li>
+            <li>발표</li>
+          </ul>
+        </details>
+      </div>
+      <div className="but">
+        <button>과목추가</button>
+      </div>
+    </TaskContainer>
+  );
 };
 
 export default Task;
